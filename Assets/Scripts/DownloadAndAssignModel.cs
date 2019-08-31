f010112c66fe6e56bb6c1ddcdbe87199c32f5546
Keenan Gray <keenangray@Keenans-MBP-2.tampabay.rr.com>
2019-08-31 13:20:59 -0400

    Initialize Repo

---
+++ Assets/Scripts/DownloadAndAssignModel.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityGoogleDrive;
+using System.Text;
+using System.IO;
+using UnityEditor;
+using TriLib;
+
+public class DownloadAndAssignModel : MonoBehaviour
+{
+    private GoogleDriveFiles.DownloadRequest request;
+    private string result = string.Empty;
+
+    private string fileId;
+    string fileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(DownloadModel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetFileId(string newId)
+    {
+        fileId = newId;
+    }
+    public void SetFileName(string newName)
+    {
+        fileName = newName;
+    }
+
+    void DownloadModel()
+    {
+        var path = Application.persistentDataPath + "/" + fileName.Replace(" ", "");
+        //EditorUtility.RevealInFinder(path);
+
+        //first try to get the file by name
+        try
+        {
+            using (var assetLoader = new AssetLoader())
+            {
+                var ops = AssetLoaderOptions.CreateInstance();   //Creates the AssetLoaderOptions instance.
+                                                                 //AssetLoaderOptions let you specify options to load your model.
+                                                                 //(Optional) You can skip this object creation and it's parameter or pass null.
+
+                //You can modify assetLoaderOptions before passing it to LoadFromFile method. You can check the AssetLoaderOptions API reference at:
+                //https://ricardoreis.net/trilib/manual/html/class_tri_lib_1_1_asset_loader_options.html
+                ops.UseOriginalPositionRotationAndScale = true;
+                ops.Scale = 1 / 1000f;
+
+                var wrapperGameObject = gameObject;                             //Sets the game object where your model will be loaded into.
+                                                                                //(Optional) You can skip this object creation and it's parameter or pass null.
+
+                var myGameObject = assetLoader.LoadFromFile(path, ops, wrapperGameObject); //Loads the model synchronously and stores the reference in myGameObject.
+
+
+                //successful retreival of file
+                if (myGameObject != null)
+                {
+                    var ObjectToPlace = Instantiate(myGameObject);
+
+                    //After instantiating object kill UI
+                    GameObject.Find("Canvas").SetActive(false);
+
+                    //Assign variable to TapToPlace
+                    GameObject.Find("AR Session Origin").GetComponent<TapToPlace>().objectToPlace = ObjectToPlace;
+
+                }
+            }
+
+        }
+        catch
+        {
+            //if we don't get the file locally, we'll have to download it from the server
+
+            request = GoogleDriveFiles.Download(fileId);
+
+            //request = GoogleDriveFiles.Download(fileId, range.start >= 0 ? (RangeInt?)range : null);
+            request.Send().OnDone += SetResult;
+        }
+
+
+    }
+
+    private void SetResult(UnityGoogleDrive.Data.File file)
+    {
+        result = Encoding.UTF8.GetString(file.Content);
+        var path = Application.persistentDataPath + "/" + fileName.Replace(" ", "");
+
+        File.WriteAllBytes(path, file.Content);
+
+        //Assign the file to the placement object
+        //successful retreival of file
+        using (var assetLoader = new AssetLoader())
+        {
+            var ops = AssetLoaderOptions.CreateInstance();   //Creates the AssetLoaderOptions instance.
+                                                             //AssetLoaderOptions let you specify options to load your model.
+                                                             //(Optional) You can skip this object creation and it's parameter or pass null.
+
+            //You can modify assetLoaderOptions before passing it to LoadFromFile method. You can check the AssetLoaderOptions API reference at:
+            //https://ricardoreis.net/trilib/manual/html/class_tri_lib_1_1_asset_loader_options.html
+            ops.UseOriginalPositionRotationAndScale = true;
+            ops.Scale = 1 / 1000f;
+
+            var wrapperGameObject = gameObject;                             //Sets the game object where your model will be loaded into.
+                                                                            //(Optional) You can skip this object creation and it's parameter or pass null.
+
+            var myGameObject = assetLoader.LoadFromFile(path, ops, wrapperGameObject); //Loads the model synchronously and stores the reference in myGameObject.
+            var ObjectToPlace = Instantiate(myGameObject);
+
+            if (ObjectToPlace != null)
+            {
+                //After instantiating object kill UI
+                GameObject.Find("Canvas").SetActive(false);
+
+                //Assign variable to TapToPlace
+                GameObject.Find("AR Session Origin").GetComponent<TapToPlace>().objectToPlace = ObjectToPlace;
+            }
+        }
+
+
+        //EditorUtility.RevealInFinder(path);
+    }
+
+
+}
---
+++ Assets/Scripts/GenerateListOfModles.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGoogleDrive;
+
+public class GenerateListOfModles : MonoBehaviour
+{
+
+    private GoogleDriveFiles.ListRequest request;
+    private Dictionary<string, string> results;
+    private string query = string.Empty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ListFiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ListFiles(string nextPageToken = null)
+    {
+        request = GoogleDriveFiles.List();
+        request.Fields = new List<string> { "nextPageToken, files(id, name, size, createdTime)" };
+        //		request.PageSize = ResultsPerPage;
+        if (!string.IsNullOrEmpty(query))
+            request.Q = string.Format("name contains '{0}'", query);
+        if (!string.IsNullOrEmpty(nextPageToken))
+            request.PageToken = nextPageToken;
+        request.Send().OnDone += BuildResults;
+
+    }
+
+
+    private void BuildResults(UnityGoogleDrive.Data.FileList fileList)
+    {
+        results = new Dictionary<string, string>();
+
+        foreach (var file in fileList.Files)
+        {
+            var fileInfo = string.Format("Name: {0}, Size: {1:0.00}MB, Created: {2:dd.MM.yyyy}",
+                file.Name,
+                file.Size * .000001f,
+                file.CreatedTime);
+            results.Add(file.Id, fileInfo);
+        }
+
+        GenerateScrollMenuButtons(results);
+
+
+    }
+
+
+    void GenerateScrollMenuButtons(Dictionary<string, string> res)
+    {
+        var objPool = GameObject.Find("ScrollMenuPool");
+        var scrollRect = GetComponentInChildren<ScrollRect>();
+
+        foreach (string s in res.Keys)
+        {
+            string s1 = null;
+            res.TryGetValue(s, out s1);
+            s1 = ParseInfo("Name", s1);
+
+            if (s1 != null)
+            {
+                var btn = objPool.transform.GetChild(0);
+                btn.transform.SetParent(scrollRect.content);
+                btn.GetComponentInChildren<TextMeshProUGUI>().text = ParseInfo("Name", s1);
+                btn.GetComponent<DownloadAndAssignModel>().SetFileId(s);
+                btn.GetComponent<DownloadAndAssignModel>().SetFileName(ParseInfo("Name", s1));
+            }
+        }
+    }
+
+    private string ParseInfo(string v, string s1)
+    {
+        var val = s1.Split(',');
+
+        int index = -1;
+
+        for (int i = 0; i < val.Length; i++)
+        {
+            if (val[i].Split(':')[0] == v)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            ///	Debug.LogError("string not found");
+        }
+        else
+        {
+            s1 = val[index].Split(':')[1];
+        }
+
+        if (s1.Split('.').Length > 0) //Does the string have a file extension
+        {
+            try
+            {
+                if (s1.Split('.')[1] == "fbx" || s1.Split('.')[1] == "obj") //is the extension fbx or obj
+                    return s1;
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.GetType() == typeof(NullReferenceException))
+                {
+
+                }
+            }
+        }
+        return null;//return an empty string for files that we don't need
+    }
+}
